8d8e3a35c8ac5c5dc464b50f8a8e7e06047e30f6
Leandro Zeni <leandro-zeni@hotmail.com>
2025-05-26 23:03:33 -0300

    Teste emulador Firebase

---
+++ app/firebaseConfig.js
@@ -0,0 +1,24 @@
+// firebaseConfig.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAbAQ6AlD9Z1CT5j5aoIVu5anwqhGS3wCE",
+  authDomain: "rotaifpr.firebaseapp.com",
+  projectId: "rotaifpr",
+  storageBucket: "rotaifpr.firebasestorage.app",
+  messagingSenderId: "1033696459638",
+  appId: "1:1033696459638:web:5acc31894d9993e9cf9857",
+  measurementId: "G-WZLK3NRW3E"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+
+  
+  
+  
+  
+  
+  
+  
